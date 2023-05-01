3e03ab35f1a31ceeebb9042fe55802b040a04f23
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-05-01 09:25:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 # Mandatory variables for the bot to start
 API_ID = int(os.getenv("API_ID", "21748181"))
 API_HASH = os.environ.get("API_HASH", "b1d962414e186e0778911f3183feac33")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6221359148:AAFm_TFyB_BG8MWGRxpVluB3f4qhi_iDX4U")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5824462361:AAHlZpwcvS__MflRmNDmWlW5mah90gZYWYQ")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("5651594253") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "mdisk")
